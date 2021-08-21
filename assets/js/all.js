77a0d728ff171370ae7820c8d42574c386df9e49
Jie <y1020329@yahoo.com.tw>
2021-08-22 00:23:12 +0800

    Update 2021-08-21T16:23:07.988Z

---
+++ assets/js/all.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+//# sourceMappingURL=all.js.map
